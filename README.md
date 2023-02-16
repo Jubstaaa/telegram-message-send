33da4610095accc308b9b63a535be21aac525a67
İlker Balcılar <ilkerbalcilartr@gmail.com>
2023-02-16 14:36:22 +0300

    add new features, update readme

--- README.md
+++ README.md
@@ -24,3 +24,19 @@ const chatId = '#########'
 
 telegramSendMessage(botkey, chatId, 'Your Amazing Message')
 ```
+
+## Create a new Telegram bot with BotFather
+
+Create a Telegram bot with BotFather before connecting your bot to Telegram.
+1. Start a new conversation with the [BotFather](https://telegram.me/botfather).
+1. Send /newbot to create a new Telegram bot.
+1. When asked, enter a name for the bot.
+1. Copy and save the Telegram bot's access token for later steps.
+
+## Get your Get Your Telegram Chat ID
+
+1. In your Telegram account, search for “@myidbot” or open this link [t.me/myidbot](https://t.me/myidbot) on your smartphone.
+1. Start a conversation with that bot and type /getid. You will get a reply back with your user ID. 
+1. Note the user id.
+ > In order to receive a message from the bot, you must first send a message to the bot and create a chat.
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,153 @@
 export const telegramSendMessage = (botKey: string, chatId: string, text: string) => {
-  fetch(`https://api.telegram.org/bot${botKey}/sendMessage?chat_id=${chatId}&text=${text}`)
-    .then((res) => {
-      console.log(res)
-      return true
-    })
-    .catch((err) => {
-      console.log(err)
+  fetch(`https://api.telegram.org/bot${botKey}/sendMessage?chat_id=${chatId}&text=${text}`).then((res) => {
+    if (res.status > 400) {
       return false
-    })
+    } else if (res.status > 200) {
+      return true
+    }
+  })
+}
+
+export const telegramSendPhoto = (botKey: string, chatId: string, imageURL: string, caption = undefined) => {
+  fetch(
+    `https://api.telegram.org/bot${botKey}/sendPhoto?chat_id=${chatId}&photo=${imageURL}${
+      caption ? `&caption=${caption}` : ''
+    }`,
+  ).then((res) => {
+    if (res.status > 400) {
+      return false
+    } else if (res.status > 200) {
+      return true
+    }
+  })
+}
+
+export const telegramSendAudio = (botKey: string, chatId: string, audioURL: string, caption = undefined) => {
+  fetch(
+    `https://api.telegram.org/bot${botKey}/sendAudio?chat_id=${chatId}&audio=${audioURL}${
+      caption ? `&caption=${caption}` : ''
+    }`,
+  ).then((res) => {
+    if (res.status > 400) {
+      return false
+    } else if (res.status > 200) {
+      return true
+    }
+  })
+}
+
+export const telegramSendDocument = (botKey: string, chatId: string, videoURL: string, caption = undefined) => {
+  fetch(
+    `https://api.telegram.org/bot${botKey}/sendDocument?chat_id=${chatId}&document=${videoURL}${
+      caption ? `&caption=${caption}` : ''
+    }`,
+  ).then((res) => {
+    if (res.status > 400) {
+      return false
+    } else if (res.status > 200) {
+      return true
+    }
+  })
+}
+
+export const telegramSendVideo = (botKey: string, chatId: string, videoURL: string, caption = undefined) => {
+  fetch(
+    `https://api.telegram.org/bot${botKey}/sendVideo?chat_id=${chatId}&video=${videoURL}${
+      caption ? `&caption=${caption}` : ''
+    }`,
+  ).then((res) => {
+    if (res.status > 400) {
+      return false
+    } else if (res.status > 200) {
+      return true
+    }
+  })
+}
+
+export const telegramSendGIF = (botKey: string, chatId: string, gifURL: string, caption = undefined) => {
+  fetch(
+    `https://api.telegram.org/bot${botKey}/sendAnimation?chat_id=${chatId}&animation=${gifURL}${
+      caption ? `&caption=${caption}` : ''
+    }`,
+  ).then((res) => {
+    if (res.status > 400) {
+      return false
+    } else if (res.status > 200) {
+      return true
+    }
+  })
+}
+
+export const telegramSendLocation = (botKey: string, chatId: string, latitude: number, longitude: number) => {
+  fetch(
+    `https://api.telegram.org/bot${botKey}/sendLocation?chat_id=${chatId}&latitude=${latitude}&longitude=${longitude}`,
+  ).then((res) => {
+    if (res.status > 400) {
+      return false
+    } else if (res.status > 200) {
+      return true
+    }
+  })
+}
+
+export const telegramSendContact = (botKey: string, chatId: string, name: string, phone_number: string) => {
+  fetch(
+    `https://api.telegram.org/bot${botKey}/sendContact?chat_id=${chatId}&first_name=${name}&phone_number=${phone_number}`,
+  ).then((res) => {
+    if (res.status > 400) {
+      return false
+    } else if (res.status > 200) {
+      return true
+    }
+  })
+}
+
+export const telegramSendPoll = (botKey: string, chatId: string, question: string, options: Array<string>) => {
+  fetch(
+    `https://api.telegram.org/bot${botKey}/sendPoll?chat_id=${chatId}&question=${question}&options=${JSON.stringify(
+      options,
+    )}`,
+  ).then((res) => {
+    if (res.status > 400) {
+      return false
+    } else if (res.status > 200) {
+      return true
+    }
+  })
+}
+
+export const telegramSendDice = (botKey: string, chatId: string, type: string) => {
+  let emoji = '🎲'
+  switch (type) {
+    case 'dice':
+      emoji = '🎲'
+      break
+    case 'dart':
+      emoji = '🎯'
+      break
+    case 'basketball':
+      emoji = '🏀'
+      break
+    case 'football':
+      emoji = '⚽'
+      break
+    case 'bowling':
+      emoji = '🎳'
+      break
+    case 'slot':
+      emoji = '🎰'
+      break
+    default:
+      emoji = '🎲'
+      break
+  }
+  fetch(`https://api.telegram.org/bot${botKey}/sendDice?chat_id=${chatId}${type ? `&emoji=${emoji}` : ''}`).then(
+    (res) => {
+      if (res.status > 400) {
+        return false
+      } else if (res.status > 200) {
+        return true
+      }
+    },
+  )
 }
